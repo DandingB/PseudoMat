c76f3f9dc4bbd05a28b4f9b8a60c2ffe7ced2616
Alb9ert <alberthansen2800@gmail.com>
2025-03-24 12:02:20 +0100

    til M

--- main.ml
+++ main.ml
@@ -1,17 +1,25 @@
 open Lexing
 open Parser
 open Lexer
+open Ast 
 
-let () = 
-    let c = open_in Sys.argv.(1) in 
-    let lexbuf = Lexing.from_channel c in
-    let result = Parser.main Lexer.token lexbuf in
-
-let rec printTree = function 
-  | Ecst x -> Printf.printf "Ecst %d" x
+let rec printTree expr =
+  match expr with
+  | Ecst (Cint x) -> Printf.printf "Ecst %d\n" x
   | Ebinop (op, e1, e2) ->
-      Printf.printf "Binop {";
       printTree e1;
-      Printf.printf " %s " op;
+      (match op with
+      | Badd -> Printf.printf " + "
+      | Bsub -> Printf.printf " - "
+      | Bmul -> Printf.printf " * "
+      | Bdiv -> Printf.printf " / "
+      | _ -> Printf.printf " ?");
       printTree e2;
-      Printf.printf "}"
+      Printf.printf "\n"
+  | _ -> Printf.printf "Unknown expression\n"
+
+let () =
+  let c = open_in Sys.argv.(1) in
+  let lexbuf = Lexing.from_channel c in
+  let result = Parser.main Lexer.token lexbuf in
+  printTree result
