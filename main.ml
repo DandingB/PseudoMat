82aba9f00000092ae25d4aee084d470fee96ea3e
Emil Lykke Grann <emillykkeg@gmail.com>
2025-04-16 11:14:25 +0200

    fixed

--- main.ml
+++ main.ml
@@ -4,13 +4,6 @@ open Lexer
 open Ast 
 open Interp
 
-(* let () =
-  let c = open_in Sys.argv.(1) in
-  let lexbuf = Lexing.from_channel c in
-  let result = Parser.main Lexer.next_token lexbuf in
-
-  Interp.file result *)
-
 let () =
 (* Read characters from file *)
 let lexbuf = Lexing.from_channel (open_in Sys.argv.(1)) in
